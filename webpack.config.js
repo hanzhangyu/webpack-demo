01dc39b0fc625a5dec2ac85e6847e8d94585283f
PaulHan <zhangyu.han@outlook.com>
2019-08-21 02:32:14 +0800

    plugin：设置 concatenateModules 启用 webpack.optimize.ModuleConcatenationPlugin，将所有模块编译至 一个闭包中

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ManifestPlugin = require("webpack-manifest-plugin");
 // const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
     app: "./src/index.js"
   },
@@ -14,12 +14,16 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  optimization: {
+    concatenateModules: true
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "管理输出"
     }),
-    new ManifestPlugin()
+    new ManifestPlugin(),
+    // new webpack.optimize.ModuleConcatenationPlugin() // 已被废弃了，推荐 optimization.concatenateModules
     // new webpack.HotModuleReplacementPlugin()
   ]
 };
