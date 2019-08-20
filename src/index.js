0a5d268adbe3061840d65328e4741a342f65f126
PaulHan <zhangyu.han@outlook.com>
2019-08-21 00:51:00 +0800

    模块热替换：启用 HMR
    module.hot.accept 钩子函数中进行更新后的操作，TODO 检查原理

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import _ from "lodash";
-import printMe from './print.js';
+import printMe from "./print.js";
 
 function component() {
   var element = document.createElement("div");
-  var btn = document.createElement('button');
+  var btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  btn.innerHTML = '点击这里，然后查看 console！';
+  btn.innerHTML = "点击这里，然后查看 console！";
   btn.onclick = printMe;
 
   element.appendChild(btn);
@@ -16,3 +16,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept("./print.js", function() {
+    console.log("Accepting the updated printMe module!");
+    printMe();
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
   entry: {
-    app: "./src/index.js",
-    print: "./src/print.js"
+    app: "./src/index.js"
   },
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
+    hot: true
   },
   output: {
     filename: "[name].bundle.js",
@@ -23,6 +24,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "管理输出"
     }),
-    new ManifestPlugin()
+    new ManifestPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
