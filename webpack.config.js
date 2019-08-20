e836a845b3ce31200e026f13c4685ec5a85db1fe
PaulHan <zhangyu.han@outlook.com>
2019-08-21 05:20:50 +0800

    创建 library: 使用 externals 把 lodash 当作 peerDependency

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "webpack-numbers.js"
+  },
+  externals: {
+    lodash: {
+      commonjs: "lodash", // 可以将 library 作为一个 CommonJS 模块访问。
+      commonjs2: "lodash", // 和上面的类似，但导出的是 module.exports.default.
+      amd: "lodash", // 类似于 commonjs，但使用 AMD 模块系统。
+      root: "_" // 可以通过一个全局变量访问 library（例如，通过 script 标签）。
+    }
   }
 };
