35df549a5bd189106dab5427cbf17bd6c37ff96c
PaulHan <zhangyu.han@outlook.com>
2019-08-21 01:05:10 +0800

    模块热替换：HMR 加载样式，会自动更新
    TODO 学习 vue-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import printMe from "./print.js";
+import "./styles.css"; // style 会自动更新
 
 function component() {
   var element = document.createElement("div");
@@ -15,11 +16,16 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept("./print.js", function() {
-    console.log("Accepting the updated printMe module!");
-    printMe();
+    console.log('Accepting the updated printMe module!');
+    // region 重新渲染来更新 事件绑定
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+    // endregion
   });
 }
