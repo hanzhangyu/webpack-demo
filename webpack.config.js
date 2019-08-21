0428b3302bd3d3726d310decf1e87aec67fff70a
PaulHan <zhangyu.han@outlook.com>
2019-08-22 05:55:50 +0800

    环境变量： 当 webpack 配置对象导出为一个函数时，可以使用 --env 向起传入一个 environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
-  devtool: "inline-source-map", // 告诉 webpack 提取这些 source map，并内联到最终的 bundle 中。
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader:"css-loader",
-            options: {
-              modules: true
+module.exports = env => {
+  // Use env.<YOUR VARIABLE> here: 这里我们并没有使用
+  console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+  console.log('Production: ', env.production); // true
+
+  return {
+    mode: "development",
+    entry: "./src/index.ts",
+    devtool: "inline-source-map", // 告诉 webpack 提取这些 source map，并内联到最终的 bundle 中。
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: true
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    }
+  };
 };
