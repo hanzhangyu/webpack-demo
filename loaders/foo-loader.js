6d2ea54051055ad3726f2c9af9965df9aa443e66
PaulHan <zhangyu.han@outlook.com>
2019-08-25 14:36:55 +0800

    chore: format

--- loaders/foo-loader.js
+++ loaders/foo-loader.js
@@ -24,11 +24,11 @@ module.exports = function(source) {
   const options = getOptions(this);
   validateOptions(schema, options, "Example Loader");
 
-  const depPath = path.resolve(__dirname, 'dep.js');
+  const depPath = path.resolve(__dirname, "dep.js");
   this.addDependency(depPath); // 外部资源必须声明，使缓存 loaders 无效，以及 添加 watch
 
-  fs.readFile(depPath, 'utf-8', function(err, header) {
-    if(err) return callback(err);
-    callback(null, header + '\n' + source); // 补上一段代码在文件开头，在 sourceMap 中为 entry 文件的 第一行
+  fs.readFile(depPath, "utf-8", function(err, header) {
+    if (err) return callback(err);
+    callback(null, header + "\n" + source); // 补上一段代码在文件开头，在 sourceMap 中为 entry 文件的 第一行
   });
 };
