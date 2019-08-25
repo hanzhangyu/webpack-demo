f2d61467cc346fe2f2b087e809acdb17d9be33e2
PaulHan <zhangyu.han@outlook.com>
2019-08-25 14:00:08 +0800

    LOADER：通过 addDependency  添加 loader依赖
    - loader 文件本身不会被 watch
        - 虽然也可以对 loader 文件自身使用 addDependency，但是并不会引起 loader 更新，只会触发使用内存中的loader 函数重编译而已
    - 添加了依赖的文件，会被 watch，并使 cache 失效而得到更新

--- loaders/foo-loader.js
+++ loaders/foo-loader.js
@@ -1,3 +1,9 @@
+/**
+ * 自定义的loader，不会被 watch 监听
+ * @type {module:path}
+ */
+const path = require("path");
+const fs = require("fs");
 const { getOptions } = require("loader-utils");
 const validateOptions = require("schema-utils");
 
@@ -11,11 +17,18 @@ const schema = {
 };
 
 module.exports = function(source) {
-  const options = getOptions(this);
-
   console.log(source);
 
+  const callback = this.async();
+
+  const options = getOptions(this);
   validateOptions(schema, options, "Example Loader");
 
-  return source;
+  const depPath = path.resolve(__dirname, 'dep.js');
+  this.addDependency(depPath); // 外部资源必须声明，使缓存 loaders 无效，以及 添加 watch
+
+  fs.readFile(depPath, 'utf-8', function(err, header) {
+    if(err) return callback(err);
+    callback(null, header + '\n' + source); // 补上一段代码在文件开头，在 sourceMap 中为 entry 文件的 第一行
+  });
 };
