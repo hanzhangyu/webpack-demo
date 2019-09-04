c835be5f9c487da8b9edbaf34fcd474562dcbe6f
PaulHan <han.zhangyu@ricequant.com>
2019-09-04 13:57:57 +0800

    babel: 忽略 babelrc

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@
  * 项目范围的 babel config 会被 ./babelrc 中的 配置给 override
  */
 module.exports = {
-  // babelrc: false, // 屏蔽 ./.babelrc
+  babelrc: false, // 屏蔽 ./.babelrc
   presets: [
     [
       "@babel/preset-env",
